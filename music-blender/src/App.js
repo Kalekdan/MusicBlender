6d144c43743b5dd568c480ae1e93542862153700
Joe Rickard <joerickard@hotmail.co.uk>
2022-12-06 16:37:25 +0000

    Added BMAC button

--- music-blender/src/App.js
+++ music-blender/src/App.js
@@ -1,22 +1,25 @@
-import './App.css';
-import {PlayButton} from './PlayButton';
-import {ExportButton} from './ExportButton';
-import {LoadButton} from './LoadButton';
+import "./App.css";
+import { PlayButton } from "./PlayButton";
+import { ExportButton } from "./ExportButton";
+import { LoadButton } from "./LoadButton";
 import React, { useState } from "react";
+import Coffee from "./Coffee";
 
 function App() {
   const [scenes, setScenes] = useState([]);
 
   return (
     <div className="App">
-      <ExportButton></ExportButton>
-      <LoadButton scenes={scenes} setScenes={setScenes}></LoadButton>
+      <div>
+        <ExportButton></ExportButton>
+        <LoadButton scenes={scenes} setScenes={setScenes}></LoadButton>
+      </div>
       <header className="App-header">
         <PlayButton scenes={scenes} setScenes={setScenes}></PlayButton>
       </header>
-      
+      <Coffee></Coffee>
     </div>
   );
 }
- 
+
 export default App;
